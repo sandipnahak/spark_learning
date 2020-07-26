67cd242d34fd0fcde88958fcc4b11b84cd70ad16
Nahak, Sandip <Sandip_Nahak@intuit.com>
2020-07-26 22:47:43 +0530

    updating

--- flight_data_analysis.py
+++ flight_data_analysis.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 
 def run_job():
     spark = SparkSession.builder.appName('flight_data').getOrCreate()
-    df = spark.read.json("s3a://analytics-learning-bucket-us-west-2/data/data/flight-data/json/2015-summary.json")
+    df = spark.read.json("s3a://analytics-learning-bucket-us-west-2/data/flight-data/json/2015-summary.json")
     df.printSchema()
 
 
